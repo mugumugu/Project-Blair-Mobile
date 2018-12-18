390c26f973fbd5142e6481f72fae8bd25a5c609d
Darryl <>
2018-12-18 20:02:35 +0800

    File Add

---
+++ Assets/Assets/Controls/screenJoystick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class screenJoystick : MonoBehaviour {
+	public float moveSpeed = 8f;
+    public FloatingJoystick joystickMove,joystickLook;
+	public FirstPersonController firstPerson;
+	public Camera c;
+
+	void Start(){
+		c = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+
+	void Update () 
+	{
+		firstPerson.horizontal = joystickMove.Horizontal;
+		firstPerson.vertical = joystickMove.Vertical;
+
+		rotateCamera();
+	}
+
+	Quaternion originalRotation;
+
+	float rotationX = 0,
+		  rotationY = 0;
+
+	void rotateCamera(){
+		rotationX += joystickLook.Horizontal * moveSpeed;
+		rotationY += joystickLook.Vertical * moveSpeed;
+
+		c.transform.localEulerAngles = new Vector3(-rotationY,0,0);
+		firstPerson.transform.localEulerAngles = new Vector3(0,rotationX,0);
+
+	}
+}
